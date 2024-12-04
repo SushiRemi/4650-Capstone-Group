7d5ecf5618f0cb1ea011b347fdaf6c7b18496b3c
djRemskii <jeremyong@cpp.edu>
2024-12-04 11:51:42 -0800

    Preparing to track stats for average calculation

--- pokemon-stat-comparison.py
+++ pokemon-stat-comparison.py
@@ -1,16 +1,21 @@
 from mrjob.job import MRJob
 
 class PokemonStatMapping(MRJob):
-
-    ##key = ","
+    pkmnCount = 0
+    totalHP = 0
+    totalAtt = 0
+    totalDef = 0
+    totalSAtt = 0
+    totalSDef = 0
+    totalSpd = 0
+    
+    ##splits file by line
     def mapper(self, _, line):
         thelist = line.split("/n")
         for x in thelist:
-            ##Split items into array
-            ##pokemon = x.split(",")
-            ##yield pokemon[1], pokemon[4]
             yield x, 1
 
+    ##reduces data to just id/name and stat values.
     def reducer(self, key, values):
         pokemon = key.split(",")
         pokemonID = pokemon[0] + " - " + pokemon[1]
@@ -18,18 +23,34 @@ class PokemonStatMapping(MRJob):
         lastIndex = len(pokemon) - 1
         pokemonStats = pokemonStats = pokemon[lastIndex-5] + "," + pokemon[lastIndex-4] + "," + pokemon[lastIndex-3] + "," + pokemon[lastIndex-2] + "," + pokemon[lastIndex-1] + "," + pokemon[lastIndex]
 
-        
-        """
-        if(pokemon[4].isdigit()):
-            checkPKMN4 = int(pokemon[4])
-            pokemonStats = pokemon[4] + "," + pokemon[5] + "," + pokemon[6] + "," + pokemon[7] + "," + pokemon[8] + "," + pokemon[9]
-        else:
-            pokemonStats = pokemon[5] + "," + pokemon[6] + "," + pokemon[7] + "," + pokemon[8] + "," + pokemon[9] + "," + pokemon[10]
-        """
-
+        if(pokemon[0] != "No"): ##To ensure header is not counted
+            self.pkmnCount += 1
+            self.totalHP += int(pokemon[lastIndex-5])
+            self.totalAtt += int(pokemon[lastIndex-4])
+            self.totalDef += int(pokemon[lastIndex-3])
+            self.totalSAtt += int(pokemon[lastIndex-2])
+            self.totalSDef += int(pokemon[lastIndex-1])
+            self.totalSpd += int(pokemon[lastIndex])
         
         yield pokemonID, pokemonStats
         ##yield key, sum(values)
+    print(pkmnCount)
+    
+    """
+    avgHP = totalHP/pkmnCount
+    avgAtt = totalAtt/pkmnCount
+    avgDef = totalDef/pkmnCount
+    avgSAtt = totalSAtt/pkmnCount
+    avgSDef = totalSDef/pkmnCount
+    avgSpeed = totalSpd/pkmnCount
+
+    print("Average HP: " + str(avgHP))
+    print("Average Attack: " + str(avgAtt))
+    print("Average Defense: " + str(avgDef))
+    print("Average Special Attack: " + str(avgSAtt))
+    print("Average Special Defense: " + str(avgSDef))
+    print("Average Speed: " + str(avgSpd))
+    """
 
 if __name__ == '__main__':
     PokemonStatMapping.run()
